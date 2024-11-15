4e3fe62d8477ffd3a90734a72d6df1d67450e882
Yudhitya Suarbawa <he.is_tya2@hotmail.com>
2024-11-15 14:01:11 +0800

    #2

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 (function(){
-  angular.module('myFirstApp', []).controller('myFirstController', function(){
-
+  'use strict'; //limit the variable as non global usage
+  angular.module('myFirstApp', []).controller('myFirstController', function($scope){
+    $scope.name = "Yuditya";
+    $scope.greeting = function(){
+      return "Hello Coursera";
+    }
   });
 })();
